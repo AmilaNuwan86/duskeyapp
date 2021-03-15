a77140ec53af4143d12ee9ba0ef3a5ba8c29228d
Amila-Pathberiya <32818076+amila1986@users.noreply.github.com>
2021-03-15 23:19:32 +0530

    Updates

---
+++ precache-manifest.2a94fb5ed830f4f19850425fa868253d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf54628b68899e5abf6f5096923b706",
+    "url": "/duskeyapp/index.html"
+  },
+  {
+    "revision": "238a49318c75366bc620",
+    "url": "/duskeyapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/duskeyapp/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/duskeyapp/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238a49318c75366bc620",
+    "url": "/duskeyapp/static/js/main.6b8893d0.chunk.js"
+  },
+  {
+    "revision": "a3edca0b5da9a40e2344",
+    "url": "/duskeyapp/static/js/runtime-main.57d9d214.js"
+  }
+]);
